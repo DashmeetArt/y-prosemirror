19c29f88080307b803554fdc72b621fc777d9dc4
Dashmeet Singh <dashmeet2011@gmail.com>
2025-01-02 03:18:50 +0530

    Update sync-plugin.js to support modification in element attributes

--- src/plugins/sync-plugin.js
+++ src/plugins/sync-plugin.js
@@ -713,19 +713,23 @@ export const createNodeFromYElement = (
       .forEach(createChildren)
   }
   try {
-    let attrs = {}
+    const oldAttrs = el.getAttributes(prevSnapshot);
+    const newAttrs = el.getAttributes(snapshot);
+    let attrs = newAttrs;
     if (snapshot !== undefined) {
       if (!isVisible(/** @type {Y.Item} */ (el._item), snapshot)) {
-        attrs = el.getAttributes(prevSnapshot) // attributes from previous snapshot so that we can show the removed items
+        attrs = oldAttrs // attributes from previous snapshot so that we can show the removed items
         attrs.ychange = computeYChange
           ? computeYChange('removed', /** @type {Y.Item} */ (el._item).id)
           : { type: 'removed' }
       } else if (!isVisible(/** @type {Y.Item} */ (el._item), prevSnapshot)) {
-        attrs = el.getAttributes(snapshot) // attributes from latest snapshot so that we can show the added items
+        attrs = newAttrs // attributes from latest snapshot so that we can show the added items
         attrs.ychange = computeYChange
           ? computeYChange('added', /** @type {Y.Item} */ (el._item).id)
           : { type: 'added' }
       }
+    } else {
+      attrs.attrs = oldAttrs
     }
     const node = schema.node(el.nodeName, attrs, children)
     mapping.set(el, node)
